e8a4a23f5a3a7968b7cf92d20734f74e4730eb86
kirega <joseph.mutiga@gmail.com>
2019-01-11 00:35:06 +0300

    [finishes #162600149] User can delete a view

--- ui/static/incidences.js
+++ ui/static/incidences.js
@@ -1,6 +1,7 @@
 window.onload = function getIncidents() {
     get('/incidents')
         .then((values) => {
+
             var table = document.getElementById('incident_record');
             fields = ['id', 'incidenttype', 'comment', 'location', 'status'];
             for (var data of values) {
@@ -11,12 +12,12 @@ window.onload = function getIncidents() {
                     return td
                 })
                 if (data.images.length > 0 || data.videos.length > 0) {
-                        tr.insertCell(-1).innerHTML = "Yes";
-                        tr.insertCell(-1).innerHTML = "<a href=#> <i class='fas fa-trash'  data-href=" + data.id + " ></i></a> <a href=# class='view' > <i class='fas fa-eye' data-href=" + data.id + " ></i></a>";    
-                    } else {
-                        tr.insertCell(-1).innerHTML = "No";
-                        tr.insertCell(-1).innerHTML = "<a href=#> <i class='fas fa-trash'  data-href=" + data.id + " ></i></a> <a href=# class='view' > <i class='fas fa-eye'  data-href=" + data.id + " ></i></a>"; 
-                    }
+                    tr.insertCell(-1).innerHTML = "Yes";
+                    tr.insertCell(-1).innerHTML = "<a href=# onClick=removeIncident(" + data.id + ")> <i class='fas fa-trash'  data-href=" + data.id + " ></i></a> <a href=# class='view' > <i class='fas fa-eye' data-href=" + data.id + " ></i></a>";
+                } else {
+                    tr.insertCell(-1).innerHTML = "No";
+                    tr.insertCell(-1).innerHTML = "<a href=# onClick=removeIncident(" + data.id + ")> <i class='fas fa-trash'  data-href=" + data.id + " ></i></a> <a href=# class='view' > <i class='fas fa-eye'  data-href=" + data.id + " ></i></a>";
+                }
                 table.parentNode.insertBefore(tr, table.nextSibling);
             }
         })
@@ -27,9 +28,25 @@ window.onload = function getIncidents() {
                     event.preventDefault();
                     console.log(e.target.getAttribute('data-href'));
                     var id = e.target.getAttribute('data-href');
-                    localStorage.setItem('incidentId',id);
+                    localStorage.setItem('incidentId', id);
                     window.location.replace('incident.html');
                 })
             }
         })
+}
+
+function removeIncident(id) {
+    payload = {
+        method: "DELETE",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer " + localStorage.getItem('jwt')
+        }
+    }
+    return fetch(URL + "/incident/" + id, payload)
+        .then((res) => {
+            window.location.replace('incidences.html');
+            return res.json()
+        });;
 }
