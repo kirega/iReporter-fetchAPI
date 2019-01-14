9a744f7870b870b8e82bf745c2685d58131c90d4
kirega <joseph.mutiga@gmail.com>
2019-01-14 14:09:07 +0300

    [ft #162837873] Update video rendering

--- ui/static/incident.js
+++ ui/static/incident.js
@@ -4,17 +4,22 @@ window.onload = function Incident() {
     get('/incident/' + id)
         .then((data) => {
             var fields = ["status",'incidenttype', 'comment','location'];
-            console.log(data);
             fields.map((k) => {
                     var i = document.getElementById(k);
                     i.value = data[k];
                 })
             data['images'].map((k)=>{
-                var d = URL + k
-                document.querySelector('.image img').setAttribute('src',d)
-            })
+                var d = IMG_URL + k;
+                document.querySelector('.image img').setAttribute('src',d);
+                document.querySelector('.image a').setAttribute('href',d);
+            });
+        
+            data['videos'].map((k)=>{
+                var d = IMG_URL + k;
+                document.querySelector('.video').innerHTML ='<video width="420" height="315" controls src="'+d+'">\
+                Your browser does not support the video tag</video>';
+            });
             var myLatLng = new google.maps.LatLng(Number(data.location.split(',')[0]), Number(data.location.split(',')[0]));
-            console.log(myLatLng);
             var map = new google.maps.Map(document.getElementById('map'), {
                     center: {
                         lat: Number(data.location.split(',')[0]),
