4533acec55ef3a58667ce0cdd8a8c453721731b6
kirega <joseph.mutiga@gmail.com>
2019-01-24 00:39:38 +0300

    [ch #163416295] Test index.html loads fine

---
+++ __tests__/index.spec.js
@@ -0,0 +1,19 @@
+var html = require('fs').readFileSync('ui/login.html').toString();
+const puppeteer = require('puppeteer');
+let browser, page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('https://kirega.github.io/iReporter/ui/index.html');
+
+});
+afterAll(async () => {
+    await browser.close();
+});
+
+describe("index", () => {
+    it('loads', async () => {
+        expect(await page.content()).toMatch('iReporter');
+    });
+});
